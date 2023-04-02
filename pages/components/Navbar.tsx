5444521ed02ac772a7c4f0c99ccd606956550164
Jai Nanda <jainanda1411@gmail.com>
2023-04-02 23:42:20 +0530

    created the repo

---
+++ pages/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    const links = [
+        {
+          label: "Home",
+          href: "/",
+        },
+        {
+          label: "News",
+          href: "/news",
+        },
+        {
+          label: "Events",
+          href: "/events",
+        },
+        {
+          label: "Resources",
+          href: "/resources",
+        },
+        {
+          label: "The Team",
+          href: "/team",
+        },
+        {
+          label: "Contact",
+          href: "/contact",
+        },
+        {
+          label: "Gallery",
+          href: "/gallery",
+        },
+      ];
+  return (
+    <div className="max-w-7xl m-auto">
+        
+    </div>
+  )
+}

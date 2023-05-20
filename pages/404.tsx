d1861fafcdd5076a5845830cfa4c6ba988503b24
Jai Nanda <jainanda1411@gmail.com>
2023-05-20 14:08:02 +0530

    removed

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,8 +5,8 @@ export default function custom404() {
   return (
     <div className='flex justify-start items-center flex-col h-[100vh] w-full'>
     <Image className="mt-20 w-[500px] dark:from-black	" src={nice} alt="bhai"></Image>
-    <h1 className="font-semibold text-3xl">This page was not found.</h1>
-    <h1 className="font-medium text-xl mt-5">Maybe you wanna go back <Link className="text-primary-color " href={"/"}>home?</Link></h1>
+    <h1 className="sm:font-semibold sm:text-3xl text-2xl text-center">This page was not found.</h1>
+    <h1 className="font-medium text-xl mt-5 text-center">Maybe you wanna go back <Link className="text-primary-color " href={"/"}>home?</Link></h1>
 </div>
   )
 }

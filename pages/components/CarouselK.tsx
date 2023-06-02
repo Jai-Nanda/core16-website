551c080b7f9a9370b434300d21582ef466bf11f2
ArjunGambhir20099 <115174370+ArjunGambhir20099@users.noreply.github.com>
2023-06-03 02:27:13 +0530

    Update CarouselK.tsx

--- pages/components/CarouselK.tsx
+++ pages/components/CarouselK.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import hi from '../../public/news.png'
-import hi2 from '../../public/coreintras.png'
+import hi2 from '../../public/coreintras2.png'
 import Image from 'next/image';
 export default class NextJsCarousel extends Component {
 	render() {
@@ -10,7 +10,7 @@ export default class NextJsCarousel extends Component {
 			<div>
 			<Carousel>
 				<div className='sm:max-w-7xl m-auto max-w-[90%]'>
-          <Image src={hi} alt='jai'/>
+          <Image src={hi2} alt='jai'/>
 					<p className="legend">Image 1</p>
 
 				</div>
@@ -20,17 +20,17 @@ export default class NextJsCarousel extends Component {
 
 				</div>
 				<div>
-        <Image src={hi} alt='jai'/>
+        <Image src={hi2} alt='jai'/>
 					<p className="legend">Image 3</p>
 
 				</div>
 				<div>
-        <Image src={hi} alt='jai'/>
+        <Image src={hi2} alt='jai'/>
 					<p className="legend">Image 4</p>
 
 				</div>
 				<div>
-        <Image src={hi} alt='jai'/>
+        <Image src={hi2} alt='jai'/>
 					<p className="legend">Image 5</p>
 
 				</div>
